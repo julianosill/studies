46aa222c23ecb32c014e4a732c33f1a81cdfb81c
Juliano Sill <sill.juliano@gmail.com>
2022-09-15 23:56:20 -0300

    Add 2-conditional.js

---
+++ javascript/2-conditional.js
@@ -0,0 +1,43 @@
+/*	COMPARISON OPERATORS
+
+	==		equal to						// <'StringOne' == 'StringTwo'> returns <true> because both are strings.
+	===		equal value and equal type		// <'StringOne' === 'StringTwo'> returns <false> because both are strings but the value is different.
+	!=		not equal
+	!==		not equal value nor equal type
+	>		greater than
+	>		less than
+	>=		greater than or equal to
+	>=		less than or equal to
+
+*/
+
+
+// IF...ELSE STATEMENT
+
+	if(true) {		// If the condition is met, runs the code below <if>.
+		// Code here.
+	} else {		// If not, then runs the code below <else>.
+		// Another code here.
+	}
+
+
+
+/* LOGICAL OPERATORS
+
+	&&				AND operator
+	||				OR operator
+	!				NOT operator
+	FALSY values: 	0, "" or '', null, undefined, Nan (Not a Number).
+
+*/
+
+	// Some examples:
+
+	if(varOne === true && varTwo === 10) {		// Check if both of the conditionals are met, if true then runs the code.
+		// Code here.
+	}
+
+	if(varOne) {		// Check if it's TRUTHY or FALSY.
+		// Code here.
+	}
+
