4f79496730fbe4f37c5c99c1c3a728b75c565f79
Juliano Sill <sill.juliano@gmail.com>
2022-09-20 17:43:07 -0300

    Add "Default parameters" to 3-function.js

--- javascript/3-function.js
+++ javascript/3-function.js
@@ -2,10 +2,34 @@
 
 	Syntax:
 
-		function funcName() {		Create a function.
+		function funcName(parameter) {		Create a function.
 			<method here>;
 		}
 
-		funcName();					Call the function.
+		funcName(parameterValue);							Call the function.
 	
 */
+
+	// Example 1:
+	function duplicate(number) {
+		console.log('Example 1: ' + number * 2);
+	}
+	duplicate(4);
+
+
+
+/*	DEFAULT PARAMETERS
+
+	When the parameter is empty or <undefined>, the function use the default parameter that it's declared in the function.
+
+	Syntax:
+		function funcName(param = 'default value') {
+		}
+
+*/
+
+		// Example 2:
+		function sayName(name = 'Juliano') {
+			console.log(`Example 2: My name is ${name}`);
+		}
+		sayName();
