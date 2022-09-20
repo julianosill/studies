50d42a5dd796469b52e60652fd7ce8ccbd7ca311
Juliano Sill <sill.juliano@gmail.com>
2022-09-19 21:46:04 -0300

    Add "Ternary operator" to 2-conditional.js

--- javascript/2-conditional.js
+++ javascript/2-conditional.js
@@ -12,12 +12,25 @@
 */
 
 
-// IF...ELSE STATEMENT
 
-	if(true) {		// If the condition is met, runs the code below <if>.
-		// Code here.
-	} else {		// If not, then runs the code below <else>.
-		// Another code here.
+/* IF...ELSE STATEMENT
+
+	Syntax:
+
+		if(true) {		If the condition is met, runs the code below <if>.
+		} else {		If it's false, then runs the code below <else>.
+		}
+
+*/
+
+	// Example 1:
+
+	let number = 10;
+
+	if(number === 10) {
+		console.log('Example 1: The number is 10.');
+	} else {
+		console.log('The number is not 10.')
 	}
 
 
@@ -27,17 +40,51 @@
 	&&				AND operator
 	||				OR operator
 	!				NOT operator
-	FALSY values: 	0, "" or '', null, undefined, Nan (Not a Number).
+	FALSY values: 	0, "" or '', null, undefined, Nan (Not a Number)
+
+	Syntax:
+
+		if(varOne === true && varTwo === 10) {		// Check wether both of the conditionals are met and then runs the code.
+			// Code here.
+		}
+
+		if(varOne) {		// Check if it's TRUTHY or FALSY. Just use the variable name to check if it exists or has a value.
+			// Code here.
+		}
 
 */
 
-	// Some examples:
+	// Example 2 and 3:
 
-	if(varOne === true && varTwo === 10) {		// Check if both of the conditionals are met, if true then runs the code.
-		// Code here.
+	if(number > 5 || number < 15) {
+		console.log(`Example 2: The number is ${number}.`);
+	} else {
+		console.log("The number isn't in the range.");
 	}
 
-	if(varOne) {		// Check if it's TRUTHY or FALSY.
-		// Code here.
+	if(number) {
+		console.log(`Example 3: The number has a value and it's ${number}.'`);
+	} else {
+		console.log('The number is undefined.');
 	}
 
+	
+
+/* TERNARY OPERATOR
+
+	It's a short-hand syntax to simplify condition statements.
+
+	varName ? <if method> : <else method>;
+
+		- The condition is provided before the <?>.
+		- A colon <:> separates the expressions.
+		- If condition is <true>, execute the first expression.
+		- If condition is <falce>, execute the second.
+
+*/
+
+	// Example 4:
+
+	let dayTime = true;
+
+	dayTime ? console.log("It's day time.") : console.log("It's night time.");
