c86325a491dfa4c5e16ff16aca8e82de27c91e10
Juliano Sill <sill.juliano@gmail.com>
2022-09-21 20:58:22 -0300

    Add "Return" to 3-function.js

--- javascript/3-function.js
+++ javascript/3-function.js
@@ -33,3 +33,24 @@
 			console.log(`Example 2: My name is ${name}`);
 		}
 		sayName();
+
+	
+
+/*	RETURN
+
+	By default, the value of calling the function is <undefined>. You must <return> the value to capture it and print or use.
+
+	Syntax:
+		function funcName(numbOne, numbTwo) {
+			let result = numbOne + numbTwo;
+			return result;
+		}
+
+*/
+
+		// Example 3:
+		function calcArea(numbOne, numbTwo) {
+			return numbOne * numbTwo;
+		}
+		const square = calcArea(2,6);
+		console.log('Example 3: ' + square);
