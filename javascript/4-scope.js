d6ec7d9aaeca8d20f62e040aa69f9e2d8d8081a6
Juliano Sill <sill.juliano@gmail.com>
2022-09-22 21:50:13 -0300

    Add 4-scope.js

---
+++ javascript/4-scope.js
@@ -0,0 +1,13 @@
+/*	SCOPE
+
+	Defines where variables can be accessed of referenced.
+
+	GLOBAL VARIABLES	Variables that exist within global scope.
+	LOCAL VARIABLES		Variables that exist within block scope.
+	BLOCKS				It's the code inside the curly braces <{}>. For instance, a function body.
+	GLOBAL SCOPE		Variables are declared outside blocks, it can be accessed by any code wherever it's located in the file.
+	BLOCK SCOPE			It's only accessible to the block code where it's declared.
+	GLOBAL NAMESPACE	It's where global scope variables go to. They remain there until the program finishes.
+	SCOPE POLLUTION		A scope pollution is when too many variables exist in the global namespace or are reused across different scopes. It makes difficult to read and keep track variables.
+
+*/
