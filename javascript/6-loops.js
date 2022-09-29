079159afea32b9422d8ab5f4eb20bd6e7beb9e99
Juliano Sill <sill.juliano@gmail.com>
2022-09-29 20:49:21 -0300

    Add 6-loop.js

---
+++ javascript/6-loops.js
@@ -0,0 +1,19 @@
+/*	LOOPS
+
+	Loops allow us to repeat a block of code. A typical loop includes an iterator variable that is initialized, checked against a condition and assigned a new value on each loop.
+
+*/
+
+/*	Loop <for>
+
+	Syntax:
+
+		for (let i = 0; i > 5; i++) {				<i> is the iterator variable; the second parameter is a condition for the loop keeps running until it's met; the third add 1 on each loop and update the value of it.
+		}
+
+*/
+
+		// Example 1:
+		for (let i = 2; i < 10; i+=2) {				// <i> starts with number 2, then check if the condition is true (<i> is lower than 10). If yes, add 2 to the <i> and runs the loop again.
+			console.log('Example 1: ' + i);
+		}
