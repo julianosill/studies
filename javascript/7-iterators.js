0eb6b1437d222197b8fadcbded70bf4d8100a244
Juliano Sill <sill.juliano@gmail.com>
2022-10-04 21:13:16 -0300

    Add "Example 2 and 3" to 7-iterators.js

--- javascript/7-iterators.js
+++ javascript/7-iterators.js
@@ -4,14 +4,26 @@
 
 	Some examples:
 
-		.forEach()		Runs the code for each element in the array.
-		.map()
-		.filter()
+		.forEach()		Loops through the array and executes the code for each element.
+		.map()			Creates a new array with the results of calling a function for every element in array.
+		.filter()		Filters the elements that pass the condition.
 
 */
 
 	// Example 1:
 	const motorcycleBrands = ['Honda', 'Suzuki', 'Yamaha'];
 	motorcycleBrands.forEach(brand => {
-		console.log(brand + ' is a motorcycle brand.');
+		console.log('Example 1: ' + brand + ' is a motorcycle brand.');
 	});
+
+	// Example 2:
+	const smallSize = [2, 4, 6];
+	const bigSize = smallSize.map(size => size * 4);
+	console.log('Example 2 ' + bigSize);
+
+	// Example 3:
+	const numbers = ['1', 2, 'three', 4, 5 ];
+	const filterNumbers = numbers.filter(number => {		// Select only the number type data in <numbers> and save to a new array called <filterNumbers>.
+		return typeof number === 'number';
+	});
+	console.log('Example 3: ' + filterNumbers);
