db1ec304247a6911b3485d5b56a54f0419db242c
Juliano Sill <sill.juliano@gmail.com>
2022-10-05 17:52:10 -0300

    Add "Example 4" to 7-iterators.js

--- javascript/7-iterators.js
+++ javascript/7-iterators.js
@@ -6,7 +6,8 @@
 
 		.forEach()		Loops through the array and executes the code for each element.
 		.map()			Creates a new array with the results of calling a function for every element in array.
-		.filter()		Filters the elements that pass the condition.
+		.filter()		Creates a new array with elements that pass the condition.
+		.join()			Converts the elements of an array to strings and concatenated together, returning the resulting string.
 
 */
 
@@ -23,7 +24,12 @@
 
 	// Example 3:
 	const numbers = ['1', 2, 'three', 4, 5 ];
-	const filterNumbers = numbers.filter(number => {		// Select only the number type data in <numbers> and save to a new array called <filterNumbers>.
+	const filterNumbers = numbers.filter(number => {		// Select only the number type data (using <typeof>) in <numbers> and save to a new array called <filterNumbers>.
 		return typeof number === 'number';
 	});
 	console.log('Example 3: ' + filterNumbers);
+
+	// Example 4:
+	const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+	const secretMessage = animals.map(animals => animals[0]);
+	console.log('Example 4: ' + secretMessage.join(''));
