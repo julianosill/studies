47642d4886b0a4e5fec4fd948a8b0245a366c838
Juliano Sill <sill.juliano@gmail.com>
2022-10-05 19:59:50 -0300

    Named each example in 7-iterators.js

--- javascript/7-iterators.js
+++ javascript/7-iterators.js
@@ -12,30 +12,30 @@
 
 */
 
-	// Example 1:
+	// Example 1 - .forEach():
 	const motorcycleBrands = ['Honda', 'Suzuki', 'Yamaha'];
 	motorcycleBrands.forEach(brand => {
 		console.log('Example 1: ' + brand + ' is a motorcycle brand.');
 	});
 
-	// Example 2:
+	// Example 2 - .map():
 	const smallSize = [2, 4, 6];
 	const bigSize = smallSize.map(size => size * 4);
 	console.log('Example 2 ' + bigSize);
 
-	// Example 3:
+	// Example 3 - .filter():
 	const numbers = ['1', 2, 'three', 4, 5 ];
 	const filterNumbers = numbers.filter(number => {		// Select only the number type data (using <typeof>) in <numbers> and save to a new array called <filterNumbers>.
 		return typeof number === 'number';
 	});
 	console.log('Example 3: ' + filterNumbers);
 
-	// Example 4:
+	// Example 4 - map() and .join():
 	const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 	const secretMessage = animals.map(animals => animals[0]);
 	console.log('Example 4: ' + secretMessage.join(''));
 
-	// Example 5:
+	// Example 5 - .findIndex():
 	const names = ['John', 'Dave', 'Lily'];
 	const findDave = names.findIndex((name) => {
 		return name === 'Dave';
