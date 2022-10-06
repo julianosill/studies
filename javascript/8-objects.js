b9676179cc6f11ba378172fd58b7105d6fd1864e
Juliano Sill <sill.juliano@gmail.com>
2022-10-06 20:41:39 -0300

    Add 8-ojects.js

---
+++ javascript/8-objects.js
@@ -0,0 +1,18 @@
+/*	OBJECTS
+
+	It's used curly braces <{}> to designate an object literal. An object is organized into key-value pairs.
+	Key => Value.
+
+	Syntax:
+
+		let objectName = {
+			key: 'value',
+		};
+
+*/
+
+	// Example 1 - Creating an object literal:
+	let bike = {
+		brand: 'Triumph',		// "brand" and "model" are properties.
+		model: 'Tiger 900 Rally',
+	}
