668819b0e3767f0964a9dd1e7a0277c7c5251ff0
Juliano Sill <sill.juliano@gmail.com>
2022-10-06 21:13:52 -0300

    Update "Objects" description and add instructions

--- javascript/8-objects.js
+++ javascript/8-objects.js
@@ -1,13 +1,24 @@
 /*	OBJECTS
 
-	It's used curly braces <{}> to designate an object literal. An object is organized into key-value pairs.
-	Key => Value.
+	It's used curly braces <{}> to designate an object literal. An object is organized into key-value pairs (key: value) and is mutable.
+	It's possible to assign/update properties after creating an object.
 
 	Syntax:
 
 		let objectName = {
 			key: 'value',
+			'Key Name': 'value',
 		};
+
+	Accessing properties:
+
+		Dot notation		=>	objectName.key;				Returns 'value'.
+		Bracket notation	=>	objectName['Key Name'];		Returns 'value.
+
+	Assigning and update/change properties:
+
+		objectName.key = 'new value';		Updates the value of the key.
+		objectName.newKey = 'value';		Assign a new key and its value to the object.
 
 */
 
@@ -15,4 +26,8 @@
 	let bike = {
 		brand: 'Triumph',		// "brand" and "model" are properties.
 		model: 'Tiger 900 Rally',
+		'Fuel Type': 'Gas',
 	}
+	
+	bike.brand;				// Returns 'Triumph'.
+	bike['Fuel Type'];		// Returns 'Gas'.
