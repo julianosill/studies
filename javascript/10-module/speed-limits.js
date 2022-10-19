fb45a54aa7eefe02378466dd1cda66b5cc879f8a
Juliano Sill <sill.juliano@gmail.com>
2022-10-18 21:31:24 -0300

    Add comments for each code in speed-limits.js

--- javascript/10-module/speed-limits.js
+++ javascript/10-module/speed-limits.js
@@ -1,7 +1,16 @@
+// Import a module which contains a function that converts mph to kmh
 const converter = require('./converter.js');
+
+// Create an array with road types/categories
 const roadType = ['Local Street', 'Avenue', 'Highway'];
+
+// Create an array with respective speed limits
 const speedInMph = [20, 40, 60];
+
+// Create a function that converts the speed in mph to kmg and store in a new array
 const speedInKmh = speedInMph.map(speed => Math.floor(converter.convertSpeed(speed)));
+
+// Loop and print all the road types and its speed limits respectively
 for(let i = 0; i < roadType.length; i++) {
 	console.log(`The speed limit for ${roadType[i]}: ${speedInMph[i]}mph / ${speedInKmh[i]}kmh`);
 }
