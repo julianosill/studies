9289f9d77b83609937ccd137e9a675dc9e62d1bf
Juliano Sill <sill.juliano@gmail.com>
2022-10-18 21:20:51 -0300

    Add comments in converter.js

--- javascript/10-module/converter.js
+++ javascript/10-module/converter.js
@@ -1,3 +1,5 @@
+// Create a function that converts mph to kmh
 const convertToKmh = miles => miles * 1.609344;
 
+// Export that function to use it as a module
 module.exports.convertSpeed = convertToKmh;
