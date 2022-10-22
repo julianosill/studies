7a51a6baadf2cb71a6a4e85c0140a770d2660275
Juliano Sill <sill.juliano@gmail.com>
2022-10-21 21:09:01 -0300

    Add "Constructin a promise" to 11-promises.js

--- javascript/11-promises.js
+++ javascript/11-promises.js
@@ -4,13 +4,32 @@
 
 	A <Promise> object can be in one of three states:
 
-		Pending			The initial state — the operation has not completed yet.
-		Fulfilled		The operation has completed successfully and the promise now has a <resolved value>.
-		Rejected		The operation has failed and the promise has a reason for the failure. This reason is usually an Error of some kind.
+		Pending						The initial state — the operation has not completed yet.
+		Fulfilled (Resolved)		The operation has completed successfully and the promise now has a <resolved value>.
+		Rejected					The operation has failed and the promise has a reason for the failure. This reason is usually an Error of some kind.
 
 		Example:
 		Pending promise		A car running low fuel.
 		Reject				Fuel is over, the car stopped. Unable to reach the destination.
 		Resolve				There's a gas station nearby, so pull over, fill up, keep driving and reach the destination. The operation was fulfilled.
+
+
+
+	CONSTRUCTING A PROMISE
+
+		The <Promise> constructor method takes a function parameter called the <executor function> which runs automatically when the constructor is called.
+		The executor function generally starts an asynchronous operation and dictates how the promise should be settled.
+
+		The executor function has two function parameters, usually referred to as the resolve() and reject() functions.
+		The resolve() and reject() functions aren’t defined by the programmer.
+		When the Promise constructor runs, JavaScript will pass its own resolve() and reject() functions into the executor function.
+
+		<resolve>		A function with one argument. Under the hood, if invoked, resolve() will change the promise’s status from <pending> to <fulfilled>, and the promise’s resolved value will be set to the argument passed into <resolve()>.
+		<reject>		A function that takes a reason or error as an argument. Under the hood, if invoked, reject() will change the promise’s status from <pending> to <rejected>, and the promise’s rejection reason will be set to the argument passed into <reject()>.
+
+		Syntax:
+
+			const executorFunction = (resolve, reject) => { };
+			const firstPromise = new Promise(executorFunction);
 
 */
