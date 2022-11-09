d8e7938e577c8b30df25f9a04331301e12fab51a
Juliano Sill <sill.juliano@gmail.com>
2022-11-09 20:31:35 -0300

    Add 12-async-await.js

---
+++ javascript/12-async-await.js
@@ -0,0 +1,9 @@
+/*	ASYNC AWAIT
+
+	Originally, JavaScript used callback functions to handle asynchronous actions.
+	The problem with callbacks is that they encourage complexly nested code which quickly becomes difficult to read, debug, and scale.
+	With ES6, JavaScript integrated native promises which allow us to write significantly more readable code.
+	ES8 provides a new syntax for handling our asynchronous action, <async...await>.
+	The <async...await> syntax allows us to write asynchronous code that reads similarly to traditional synchronous, imperative programs.
+
+*/
