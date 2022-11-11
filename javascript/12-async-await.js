bb2b6e4ed75876656cc42af16b2fc3cc42f31206
Juliano Sill <sill.juliano@gmail.com>
2022-11-11 20:40:27 -0300

    Add "Await Promise.all()" to 12-async-await.js

--- javascript/12-async-await.js
+++ javascript/12-async-await.js
@@ -31,84 +31,105 @@
 
 */
 
-		// Example 1:
-		async function fivePromise() { 
-			return 5;
-		};
+	// Example 1:
+	async function fivePromise() { 
+		return 5;
+	};
 		
-		fivePromise()
-		.then(resolvedValue => {
-			console.log(resolvedValue);
-		});	// Prints 5
+	fivePromise()
+	.then(resolvedValue => {
+		console.log(resolvedValue);
+	});	// Prints 5
 
-		// Even though we return <5> inside the function body, what’s actually returned when we invoke fivePromise() is a promise with a resolved value of <5>.
+	// Even though we return <5> inside the function body, what’s actually returned when we invoke fivePromise() is a promise with a resolved value of <5>.
 
 
-		// Example 2:
-		let myPromise = () => {
-			return new Promise((resolve, reject) => {
-				setTimeout(() => {
-					resolve('Yay, I resolved!');
-				}, 1000);
-			});
-		};
+	// Example 2:
+	let myPromise = () => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve('Yay, I resolved!');
+			}, 1000);
+		});
+	};
 
-		async function noAwait() {
-			let value = myPromise();
-			console.log(value);
-		};
+	async function noAwait() {
+		let value = myPromise();
+		console.log(value);
+	};
 		
-		async function yesAwait() {
-			let value = await myPromise();
-			console.log(value);
-		}
+	async function yesAwait() {
+		let value = await myPromise();
+		console.log(value);
+	}
 		
-		noAwait();	// Prints "Promise { <pending> }" because the <await> keyword was not declared to the variable <value>. The <console.log()> was executed before the promise had resolved.
-		yesAwait();	// Prints "Yay, I resolved!"
+	noAwait();	// Prints "Promise { <pending> }" because the <await> keyword was not declared to the variable <value>. The <console.log()> was executed before the promise had resolved.
+	yesAwait();	// Prints "Yay, I resolved!"
 
 	
 
-	/*	TRY...CATCH
+/*	TRY...CATCH
 
-		When <.catch()> is used with a long promise chain, there is no indication of where in the chain the error was thrown.
-		This can make debugging challenging. With <async...await>, we use <try...catch> statements for error handling.
-		By using this syntax, not only are we able to handle errors in the same way we do with synchronous code, but we can also catch both synchronous and asynchronous errors. 
+	When <.catch()> is used with a long promise chain, there is no indication of where in the chain the error was thrown.
+	This can make debugging challenging. With <async...await>, we use <try...catch> statements for error handling.
+	By using this syntax, not only are we able to handle errors in the same way we do with synchronous code, but we can also catch both synchronous and asynchronous errors. 
 
-		Syntax:
+	Syntax:
 
-			async function myFunc() {
-				try {
-
-				} catch(error) {
-
-				}
+		async function myFunc() {
+			try {
+			} catch(error) {
+			}
 			};
 
 		Since async functions return promises we can still use native promise’s <.catch()> with an async function.
 
-			Example:
-			let rejectedPromise = usingPromiseCatch();
-			rejectedPromise.catch((rejectValue) => {
-				console.log(rejectValue);
-			});
+		Example:
+		let rejectedPromise = usingPromiseCatch();
+		rejectedPromise.catch((rejectValue) => {
+			console.log(rejectValue);
+		});
 
-	*/
+*/
 
-	/*	HANDLING INDEPENDENT PROMISES
+/*	HANDLING INDEPENDENT PROMISES
 
-		If possible, we want to get started on each asynchronous operation as soon as possible!
-		Within our async functions we should still take advantage of <concurrency>, the ability to perform asynchronous actions at the same time.
+	If possible, we want to get started on each asynchronous operation as soon as possible!
+	Within our async functions we should still take advantage of <concurrency>, the ability to perform asynchronous actions at the same time.
 
-		Note: if we have multiple truly independent promises that we would like to execute fully in parallel, we must use individual <.then()> functions and avoid halting our execution with <await>.
+	Note: if we have multiple truly independent promises that we would like to execute fully in parallel, we must use individual <.then()> functions and avoid halting our execution with <await>.
 
-		Syntax:
+	Syntax:
 
-			async function concurrent() {
-				const firstPromise = firstAsyncThing();
-				const secondPromise = secondAsyncThing();
-				console.log(await firstPromise, await secondPromise);
+		async function concurrent() {
+			const firstPromise = firstAsyncThing();
+			const secondPromise = secondAsyncThing();
+			console.log(await firstPromise, await secondPromise);
+		}
+
+		Both promises are constructed without <await>, then <await> each resolution to print them to the console.
+
+*/
+
+/*	AWAIT PROMISE.ALL()
+
+	Another way to take advantage of concurrency when we have multiple promises which can be executed simultaneously is to <await> a <Promise.all()>.
+	We can pass an array of promises as the argument to <Promise.all()>, and it will return a single promise.
+	This promise will resolve when all of the promises in the argument array have resolved.
+	This promise’s resolve value will be an array containing the resolved values of each promise from the argument array.
+
+	<Promise.all()> allows us to take advantage of asynchronicity — each of the asynchronous tasks can process concurrently.
+	<Promise.all()> also has the benefit of failing fast, meaning it won’t wait for the rest of the asynchronous actions to complete once any one has rejected.
+	As soon as the first promise in the array rejects, the promise returned from <Promise.all()> will reject with that reason.
+	As it was when working with native promises, <Promise.all()> is a good choice if multiple asynchronous tasks are all required, but none must wait for any other before executing.
+
+	Syntax:
+
+		async function asyncPromAll() {
+			const resultArray = await Promise.all([asyncTask1(), asyncTask2(), asyncTask3(), asyncTask4()]);
+			for (let i = 0; i<resultArray.length; i++){
+				console.log(resultArray[i]);
 			}
-
-			Both promises are constructed without <await>, then <await> each resolution to print them to the console.
+		}
 
 */
