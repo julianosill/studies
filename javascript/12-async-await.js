c0fd18c5d386c44712b490f4ffbc79dc57aecb59
Juliano Sill <sill.juliano@gmail.com>
2022-11-16 19:39:35 -0300

    Indentation corrections in 12-async.await.js

--- javascript/12-async-await.js
+++ javascript/12-async-await.js
@@ -127,7 +127,7 @@
 
 		async function asyncPromAll() {
 			const resultArray = await Promise.all([asyncTask1(), asyncTask2(), asyncTask3(), asyncTask4()]);
-			for (let i = 0; i<resultArray.length; i++){
+			for (let i = 0; i < resultArray.length; i++) {
 				console.log(resultArray[i]);
 			}
 		}
