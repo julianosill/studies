ce09c371b5cf3fc8af2a52b37970dbc376336403
Juliano Sill <sill.juliano@gmail.com>
2022-11-25 22:25:16 -0300

    style(counter): format number to start with '00'

--- js-stopwatch/script.js
+++ js-stopwatch/script.js
@@ -3,6 +3,14 @@ let min = 0;
 let hr = 0;
 let interval;
 
+const formatDigit = (number) => {
+	if(number < 10) {
+		return '0' + number;
+	} else {
+		return number;
+	}
+}
+
 const start = () => {
 	console.log('Start!');
 	interval = setInterval(counter, 1);
@@ -21,6 +29,17 @@ const stop = () => {
 
 const counter = () => {
 	sec++;
-	let currentTime = `${hr}:${min}:${sec}`;
+	if(sec==60) {
+		min++;
+		sec = 0;
+	};
+	if(min==60) {
+		hr++;
+		min = 0;
+	}
+	second = formatDigit(sec);
+	minute = formatDigit(min);
+	hour = formatDigit(hr);
+	let currentTime = `${hour}:${minute}:${second}`;
 	document.getElementById('time').innerText=currentTime;
 }
