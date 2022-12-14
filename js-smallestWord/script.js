21f778b4ef7fd55cb58e8aeaefa7d7c8d6825020
Juliano Sill <sill.juliano@gmail.com>
2022-12-13 22:44:39 -0300

    feat(smallestWord): create the function to check the smallest word

--- js-smallestWord/script.js
+++ js-smallestWord/script.js
@@ -4,6 +4,21 @@
 
 */
 
-const phrase = ' Lorem ipsum dolor sit amet at cravim do blat ';
-const trimPhrase = phrase.trim();
-const wordsArray = trimPhrase.split(' ');
+const phrase = ' Lorem ipsum dolor sit amet at cravim do blat. ';
+
+const smallestWord = (string) => {
+	if(!string || typeof string !== 'string') return 'Please, write your phrase.'
+
+	// .trim() remove any space in the beginning and end of the string | .split() separate the string in words and store in an array.
+	const wordsArray = string.trim().split(' ');
+
+	return wordsArray.reduce ((smallWord, current) => {
+		if(current.length < smallWord.length) {
+			return current;
+		}
+		return smallWord;
+	});
+
+};
+
+console.log(smallestWord(phrase));
