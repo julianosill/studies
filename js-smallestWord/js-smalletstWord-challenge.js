5f655504c2b6d21b4719506f0ebeaf67bae7e4b6
Juliano Sill <sill.juliano@gmail.com>
2022-12-13 21:43:46 -0300

    feat(var): declare variables

--- js-smallestWord/js-smalletstWord-challenge.js
+++
@@ -1,5 +0,0 @@
-/* 
-
-	Request a phrase and return the smallest word. If there are more than one, return the first one.
-
-*/
---
+++ js-smallestWord/script.js
@@ -0,0 +1,9 @@
+/* 
+
+	Request a phrase and return the smallest word. If there are more than one, return the first one.
+
+*/
+
+const phrase = ' Lorem ipsum dolor sit amet at cravim do blat ';
+const trimPhrase = phrase.trim();
+const wordsArray = trimPhrase.split(' ');
